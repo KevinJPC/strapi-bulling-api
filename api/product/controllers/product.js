e0348de2b918790d5638a6970e70216ead47402c
Kevin Pitti Castro <kepc08@gmail.com>
2021-11-22 17:29:11 -0600

    modificacion a custom endpoint

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -8,7 +8,7 @@
 module.exports = {
     findByName: async (pObj) => {
         const { name } = pObj.params;
-        const result = await strapi.query('product').search({ _q: name, _limit: 20, _start: 0 });
+        const result = await strapi.query('product').search({ _q: name, _start: 0 }, ['product']);
         return result;
     }
 };
